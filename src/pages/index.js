c736b4b1274a963933ddee383daa238babf6d517
kwonhygge <cleancode.kwon@gmail.com>
2023-06-23 11:57:28 +0900

    setting up a boilerplate code

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import Main from '../components/Main';
+import Footer from '../components/Footer';
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Create Your App</title>
+                <meta name="description" content="" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Main />
+            <Footer />
+        </div>
+    );
+}
