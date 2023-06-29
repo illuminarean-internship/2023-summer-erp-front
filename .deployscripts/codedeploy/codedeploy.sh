a7335ed1c22ddccc7a0988fcee76651141910e76
ilhyukKim <ilhyuk.kim@illuminarean.com>
2023-06-29 15:30:40 +0900

    chore: script fix path -> location

--- .deployscripts/codedeploy/codedeploy.sh
+++ .deployscripts/codedeploy/codedeploy.sh
@@ -1,12 +1,12 @@
-PATH=/home/ubuntu/
+LOCATION=/home/ubuntu/
 PROJECT=2023-internship-front
-sudo rm -rf $PATH$PROJECT\_*
-sudo mv /tmp/build $PATH$PROJECT-new
-sudo chown -R ubuntu:ubuntu $PATH$PROJECT-new
-sudo mv $PATH$PROJECT $PATH$PROJECT\_$(date +"%Y%m%d%H%M")
-sudo mv $PATH$PROJECT-new $PATH$PROJECT
+sudo rm -rf $LOCATION$PROJECT\_*
+sudo mv /tmp/build $LOCATION$PROJECT-new
+sudo chown -R ubuntu:ubuntu $LOCATION$PROJECT-new
+sudo mv $LOCATION$PROJECT $LOCATION$PROJECT\_$(date +"%Y%m%d%H%M")
+sudo mv $LOCATION$PROJECT-new $LOCATION$PROJECT
 
-pm2 list | grep $PATH$PROJECT
+pm2 list | grep $LOCATION$PROJECT
 PM2_LIST_RESULT=$?
 echo "PM2_LIST_RESULT diff a b result :  ${PM2_LIST_RESULT}"
 if [ ${PM2_LIST_RESULT} -eq "0" ]
