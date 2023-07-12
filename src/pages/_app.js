996b802780307d9dcd61d64682031e751c7f36eb
junyub.lee <junyub.lee@illuminarean.com>
2023-07-12 13:30:13 +0900

    Feat: Add navigation bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
 import GlobalStyle from '../styles/global-style';
+import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
+            <Navbar></Navbar>
             <Component {...pageProps} />{' '}
         </ThemeProvider>
     );
