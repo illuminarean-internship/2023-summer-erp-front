5f8a646cebc5c3a4cf7a3df72d643bd9d356aef0
junyub.lee <junyub.lee@illuminarean.com>
2023-07-12 13:16:02 +0900

    Test: git add test

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
                 <meta name="description" content="" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Main />
+            {/* <Main /> */}
             <Footer />
         </div>
     );
