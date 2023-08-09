730e43b0d18c0bab4b838c8b20e0d292c08d7bb9
junyub.lee <junyub.lee@illuminarean.com>
2023-08-09 09:34:19 +0900

    feat: user admin

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 
@@ -8,6 +9,25 @@ export default NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
+    callbacks: {
+        async signIn(user) {
+            // Fetch admin information from the database
+            try {
+                const response = await axios.get(
+                    `http://43.200.193.130:4040/api/users/check?email=${user.user.email}`,
+                ); // Adjust the API endpoint
+                const { isAdmin } = response.data; // Assuming the response structure has an isAdmin field
+                if (isAdmin) {
+                    return true; // Allow sign in for admin
+                } else {
+                    return false; // Deny sign in for non-admin
+                }
+            } catch (error) {
+                console.error('Error fetching admin data:', error);
+                return false; // Deny sign in on error
+            }
+        },
+    },
     secret: process.env.JWT_SECRET,
     session: {
         strategy: 'jwt',
