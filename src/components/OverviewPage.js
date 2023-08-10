e1f552beba4390929e9fbbd10fa2a56735b1dfcd
joshkimchifriedrice <josh.kim@illuminarean.com>
2023-08-09 23:37:27 -0700

    overview page updates properly, possessed items works"

--- src/components/OverviewPage.js
+++ src/components/OverviewPage.js
@@ -1,20 +1,47 @@
 import CategoryItem from './CategoryItem';
-import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import { Box, Container, Grid, Typography } from '@mui/material';
 import { v4 as uuidv4 } from 'uuid';
-
-//const startingList = [] //import from DB
-const sampleStartingList = [
-    { name: 'Accessory', link: 'accessory', size: 197 },
-    { name: 'Books', link: 'books', size: 90 },
-    { name: 'Desktop PC', link: 'desktop-pc', size: 6 },
-    { name: 'Laptop', link: 'laptop', size: 99 },
-    { name: 'Software', link: 'software', size: 9 },
-    { name: 'Test Device', link: 'test-device', size: 10 },
-];
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function OverviewPage() {
-    const { data: session } = useSession();
+    const [categorySizes, setCategorySizes] = useState([
+        '...',
+        '...',
+        '...',
+        '...',
+        '...',
+        '...',
+    ]);
+
+    const fetchData = async () => {
+        try {
+            const responses = await Promise.all([
+                axios.get('http://43.200.193.130:4040/api/accessory/'),
+                axios.get('http://43.200.193.130:4040/api/books/'),
+                axios.get('http://43.200.193.130:4040/api/test-device/'),
+                axios.get('http://43.200.193.130:4040/api/desktop-pc/'),
+                axios.get('http://43.200.193.130:4040/api/laptop/'),
+                axios.get('http://43.200.193.130:4040/api/software/'),
+            ]);
+
+            const results = await Promise.all([
+                responses[0].data.length,
+                responses[1].data.length,
+                responses[2].data.length,
+                responses[3].data.length,
+                responses[4].data.length,
+                responses[5].data.length,
+            ]);
+            setCategorySizes(results);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     // to not make any errors comment for a while
     // const [categoryButtonList, setCategoryButtonList] =
@@ -27,6 +54,15 @@ export default function OverviewPage() {
     //         size={item.size}
     //     />
     // ));
+
+    let sampleStartingList = [
+        { name: 'Accessory', link: 'accessory', size: categorySizes[0] },
+        { name: 'Books', link: 'books', size: categorySizes[1] },
+        { name: 'Desktop PC', link: 'desktop-pc', size: categorySizes[2] },
+        { name: 'Laptop', link: 'laptop', size: categorySizes[3] },
+        { name: 'Software', link: 'software', size: categorySizes[4] },
+        { name: 'Test Device', link: 'test-device', size: categorySizes[5] },
+    ];
 
     const categories = sampleStartingList.map((item) => (
         <CategoryItem item={item} key={uuidv4()} />
@@ -43,12 +79,12 @@ export default function OverviewPage() {
                     sx={{ ml: -28 }}
                 >
                     <Typography variant="h5" align="center">
-                        {`Welcome "${session.user.name}"`}
+                        {`Welcome "{session.user.name}"`}
                     </Typography>
                     <Box
                         sx={{
                             mt: 0.5,
-                            width: 170,
+                            width: 250,
                             height: 7,
                             bgcolor: 'text.disabled',
                         }}
